d060ca96b721528613e9514f4d3563c2b3ee9ed2
Simnayoung <my26920801@dongguk.edu>
2020-10-11 17:54:19 +0900

    20201011

--- Application/whatthe/app/src/main/java/com/example/whatthe/FragDash.java
+++ Application/whatthe/app/src/main/java/com/example/whatthe/FragDash.java
@@ -1,5 +1,6 @@
 package com.example.whatthe;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,30 +14,41 @@ import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 
 import java.util.ArrayList;
 
 public class FragDash extends Fragment {
     private View view;
 
-    private LineChart lineChart;
-    ArrayList<Entry> entry_chart = new ArrayList<>();
+    private LineChart chart;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater Inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
         view = Inflater.inflate(R.layout.frag_dashboard, container, false);
 
-        lineChart = (LineChart) view.findViewById(R.id.chart);//layout의 id
+        chart = (LineChart) view.findViewById(R.id.Lchart);//layout의 id
 
-        LineData chartData = new LineData();
+        ArrayList<Entry> values = new ArrayList<>();
 
-        entry_chart.add(new Entry(2,3));
-        LineDataSet lineDataSet = new LineDataSet(entry_chart,"그래프");
-        chartData.addDataSet(lineDataSet);
+        values.add(new Entry(100.0f, 0));
+        values.add(new Entry(50.0f, 0));
+        values.add(new Entry(25.0f, 0));
 
-        lineChart.setData(chartData);
-        lineChart.invalidate();
+        LineDataSet set1 = new LineDataSet(values, "DataSet 1");
+
+        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+        dataSets.add(set1); // add the data sets
+
+        ArrayList<String> xVals = new ArrayList<String>();
+        xVals.add("1.0");
+        xVals.add("2.0");
+        xVals.add("3.0");
+
+        LineData data = new LineData(xVals, dataSets);
+        chart.setData(data);
+        chart.invalidate();
 
         return view;
     }
