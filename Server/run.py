43bcedf4a0fe3fdcbcd6bfcd53592e392812a1df
ha-yujin <hoj2887@dongguk.edu>
2020-10-11 14:26:35 +0900

    merge

--- Server/run.py
+++ Server/run.py
@@ -2,18 +2,11 @@ from analysisVideo import AnalyzeVideo
 
 analyze = AnalyzeVideo()
 
-<<<<<<< HEAD
-fname = 'image/hand3.jpg'
+fname = 'image/hand2.jpg'
 
 # analyze._analyzeFace(fname)
 # analyze._analyzePose(fname)
 analyze._analyzeHand(fname)
-=======
-fname = 'image/sample5.jpg'
-
-analyze._analyzeFace(fname, 0)
-# analyze._analyzePose(fname)
->>>>>>> b1d6affefd4b43de8026bdad0f10a3b5f78445d7
 
 # from database.connect import Database
 # import datetime
@@ -21,4 +14,4 @@ analyze._analyzeFace(fname, 0)
 
 # args = (datetime.datetime.now(),1,0,0,1,0)
 # db.insertData(args)
-# db.selectAllData()
+# db.selectAllData()q
